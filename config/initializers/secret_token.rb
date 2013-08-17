e1fa9a1499183cc903f4cac732977a66629fc5df
tailang <caopingcpu@163.com>
2013-08-17 15:22:03 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thirtysunshine::Application.config.secret_token = '3b600949281924eb3e538f07d74f3780ae3cdaee476f6bac5509ae1e28f8d4d93702021b17ea881da6ea99cc2593f9931ad7672a9f558ffdbabb05b65f4f9121'
