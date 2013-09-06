9d826c64f6de92776fd78336519fde1c67dc69a7
tailang <caopingcpu@163.com>
2013-09-07 01:30:25 +0800

    registration path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Thirtysunshine::Application.routes.draw do
 
-  devise_for :users#, :controllers => { :registrations => "registrations" }
+  devise_for :users, :controllers => { :registrations => "registrations" }
   resources :users
   resources :notifications
   resources :nodes
