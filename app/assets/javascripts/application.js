e0afc6b72faf0f41d38131b9ce8131d7e88d7e39
tailang <caopingcpu@163.com>
2013-09-11 17:25:37 +0800

    add reply @user

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,12 @@
 //= require jquery_ujs
 //= require bootstrap
 //= require_tree .
+
+//点击回复，生成@
+$(function(){
+  $(document).on('click', '.at', function() {
+    var $textarea = $('#new_comment textarea');
+    $textarea.focus().val($textarea.val() + '@' + $(this).data('user-name') + ' ');
+    event.preventDefault();
+  });
+})
